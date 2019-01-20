3bfd3ebcd3e62fc2bda32b586a06f8e797f27d33
Luis Eduardo Otálora Cubides <leotalorac@unal.edu.co>
2019-01-20 12:20:33 -0500

    almost finish

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple manga downloader 
+Download manga images to creat the cbr file on a folder from [https://tmofans.com/](https://tmofans.com/) 
+
+* The program is  writen on puthon using beautiful soap and urrllib
+
+Programmed by: lwise
---
+++ project/Download.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import (
+    urlopen, urlparse, urlunparse, urlretrieve,Request,HTTPError)
+import os
+import binascii
+import struct
+import sys
+import shutil
+import time
+
+# headers fro the request
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+   'Accept-Encoding': 'none',
+   'Accept-Language': 'en-US,en;q=0.8',
+   'Connection': 'keep-alive'}
+hdr2 = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'bytes','Connection': 'keep-alive'}
+
+def main():
+	url = input("write the url \n")
+	nimg = int(input("put the number of images \n"))+1
+	name = input("Put the name of the anime\n")
+	ch = input("Put the number of the anime chapter\n")
+	newpath = "../Images/"+name.replace(" ","_") 
+	if not os.path.exists(newpath):
+		os.makedirs(newpath)
+	folder = newpath+"/"+name.replace(" ","_")+"_chapter"+str(ch)
+	if not os.path.exists(folder):
+		os.makedirs(folder)
+	for i in range(1,nimg):
+		urls = url + str(i)
+		print(urls)
+		img = downloadimage(urls,nimg,i,folder)
+		print(str(i)+".jpg")		
+		if(i%10 == 0):
+			print("wait")
+			time.sleep(30)
+	shutil.make_archive(folder+"/chapter"+str(ch), 'cbr', folder)
+def downloadimage(url,n,i,folder):
+	req = Request(url, headers=hdr)
+	try:
+		page = urlopen(req)
+		soup = bs(page,"lxml")
+		parsed = list(urlparse(url))
+		img = soup.findAll("img")
+		nameimg = iroute(i) + ".jpg"
+		image = img[0]
+		outpath = os.path.join(folder, nameimg)
+		parsed[2] = image["src"]
+		if image["src"].lower().startswith("http://"):
+			urlretrieve(image["src"], outpath)
+		else:
+			reqimg = Request(image["src"], headers=hdr2)
+			imgreq = urlopen(reqimg)
+			# Download the data.
+			with imgreq as in_file:
+				hex_data = in_file.read()
+			with open(outpath, 'wb') as out_file:
+				out_file.write(hex_data)
+	except Exception as ex :
+		print("error " + str(type(ex)) + str(ex))
+
+def iroute(i):
+	stringi = str(i);
+	ze = 3-len(stringi)
+	stringi = ("0"*ze)+stringi
+	return stringi
+if __name__ == "__main__":
+	main()
